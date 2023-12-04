acab1791db32c4ee5a326bc2ccf2baab3434d6a3
Samuel Cattini-Schultz <sam.cattini-schultz@estimateone.com>
2023-12-04 14:32:39 +1100

    Clean up 2023/day1 a bit

--- 2023/day1/main.exs
+++ 2023/day1/main.exs
@@ -1,53 +1,55 @@
 defmodule Day1 do
   # Split string into list of lines
-  def to_lines(string) do
+  defp to_lines(string) do
     String.split(string, "\n", trim: true)
   end
 
   # Return list of digits in the string, transforming spelled digits to numeric representation
-  def get_digits(string) do
+  defp get_digits(string) do
+    get_tail_digits = fn s -> get_digits(String.slice(s, 1..-1)) end
+
     case string do
-      "1" <> _ -> ["1" | get_digits(String.slice(string, 1..-1))]
-      "2" <> _ -> ["2" | get_digits(String.slice(string, 1..-1))]
-      "3" <> _ -> ["3" | get_digits(String.slice(string, 1..-1))]
-      "4" <> _ -> ["4" | get_digits(String.slice(string, 1..-1))]
-      "5" <> _ -> ["5" | get_digits(String.slice(string, 1..-1))]
-      "6" <> _ -> ["6" | get_digits(String.slice(string, 1..-1))]
-      "7" <> _ -> ["7" | get_digits(String.slice(string, 1..-1))]
-      "8" <> _ -> ["8" | get_digits(String.slice(string, 1..-1))]
-      "9" <> _ -> ["9" | get_digits(String.slice(string, 1..-1))]
-      "one" <> _ -> ["1" | get_digits(String.slice(string, 1..-1))]
-      "two" <> _ -> ["2" | get_digits(String.slice(string, 1..-1))]
-      "three" <> _ -> ["3" | get_digits(String.slice(string, 1..-1))]
-      "four" <> _ -> ["4" | get_digits(String.slice(string, 1..-1))]
-      "five" <> _ -> ["5" | get_digits(String.slice(string, 1..-1))]
-      "six" <> _ -> ["6" | get_digits(String.slice(string, 1..-1))]
-      "seven" <> _ -> ["7" | get_digits(String.slice(string, 1..-1))]
-      "eight" <> _ -> ["8" | get_digits(String.slice(string, 1..-1))]
-      "nine" <> _ -> ["9" | get_digits(String.slice(string, 1..-1))]
+      "1" <> _ -> ["1" | get_tail_digits.(string)]
+      "2" <> _ -> ["2" | get_tail_digits.(string)]
+      "3" <> _ -> ["3" | get_tail_digits.(string)]
+      "4" <> _ -> ["4" | get_tail_digits.(string)]
+      "5" <> _ -> ["5" | get_tail_digits.(string)]
+      "6" <> _ -> ["6" | get_tail_digits.(string)]
+      "7" <> _ -> ["7" | get_tail_digits.(string)]
+      "8" <> _ -> ["8" | get_tail_digits.(string)]
+      "9" <> _ -> ["9" | get_tail_digits.(string)]
+      "one" <> _ -> ["1" | get_tail_digits.(string)]
+      "two" <> _ -> ["2" | get_tail_digits.(string)]
+      "three" <> _ -> ["3" | get_tail_digits.(string)]
+      "four" <> _ -> ["4" | get_tail_digits.(string)]
+      "five" <> _ -> ["5" | get_tail_digits.(string)]
+      "six" <> _ -> ["6" | get_tail_digits.(string)]
+      "seven" <> _ -> ["7" | get_tail_digits.(string)]
+      "eight" <> _ -> ["8" | get_tail_digits.(string)]
+      "nine" <> _ -> ["9" | get_tail_digits.(string)]
       "" -> []
-      _ -> get_digits(String.slice(string, 1..-1))
+      _ -> get_tail_digits.(string)
     end
   end
 
   # Return list of digits (integer or spelled) from string
-  def filter_digits(line) do
+  defp filter_digits(line) do
     get_digits(line)
   end
 
   # Return line value from list of digits
-  def to_line_number(digit_list) do
+  defp to_line_number(digit_list) do
     String.to_integer(List.first(digit_list) <> List.last(digit_list))
   end
 
   # Return list of values from line string
-  def to_line_numbers(line_list) do
+  defp to_line_numbers(line_list) do
     line_list
     |> Enum.map(&filter_digits/1)
     |> Enum.map(&to_line_number/1)
   end
 
-  def process_input(input) do
+  defp process_input(input) do
     input
     |> to_lines()
     |> to_line_numbers()
