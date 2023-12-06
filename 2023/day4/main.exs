059e44de2dd63666a1ad7c83dd4b945158ef1ded
Samuel Cattini-Schultz <syriiins@gmail.com>
2023-12-07 02:21:37 +1100

    Add 2023/day4 part 2

--- 2023/day4/main.exs
+++ 2023/day4/main.exs
@@ -25,10 +25,16 @@ defmodule Day4 do
     |> Enum.map(&parse_card/1)
   end
 
+  defp get_matching_number_count(card) do
+    card.numbers
+    |> MapSet.intersection(card.winning_numbers)
+    |> MapSet.size()
+  end
+
   # Calculate points for card
   defp calculate_card_value(card) do
-    MapSet.intersection(card.numbers, card.winning_numbers)
-    |> MapSet.size()
+    card
+    |> get_matching_number_count()
     |> case do
       0 -> 0
       n -> 2 ** (n - 1)
@@ -41,17 +47,55 @@ defmodule Day4 do
     |> Enum.map(&calculate_card_value/1)
   end
 
+  # Zip two integer lists together, taking the sum of each indexes values
+  defp zip_sum(list1, list2) do
+    max_length = max(length(list1), length(list2))
+    padded_list1 = list1 ++ List.duplicate(0, max_length - length(list1))
+    padded_list2 = list2 ++ List.duplicate(0, max_length - length(list2))
+    Enum.zip_with(padded_list1, padded_list2, fn x, y -> x + y end)
+  end
+
+  defp do_get_card_copy_count([], _) do
+    0
+  end
+
+  # Return copies of cards using the upcoming card copies
+  defp do_get_card_copy_count(cards, copy_counts) do
+    [current_copy_count | rest_copy_counts] = copy_counts
+    [card | rest_cards] = cards
+
+    current_card_matches = get_matching_number_count(card)
+    new_upcoming_copies = List.duplicate(current_copy_count, current_card_matches)
+
+    combined_upcoming_copies = zip_sum(rest_copy_counts, new_upcoming_copies)
+    current_copy_count + do_get_card_copy_count(rest_cards, combined_upcoming_copies)
+  end
+
+  # Process cards, adding copies of subsequent cards according to winning numbers
+  defp get_card_copy_count(cards) do
+    # start with one copy per card
+    initial_copy_counts = List.duplicate(1, length(cards))
+    do_get_card_copy_count(cards, initial_copy_counts)
+  end
+
   defp process_input(scratchie_table) do
-    point_total =
+    cards =
       scratchie_table
       |> to_lines()
       |> parse_cards()
+
+    point_total =
+      cards
       |> calculate_card_values()
       |> Enum.sum()
 
+    card_copy_total =
+      cards
+      |> get_card_copy_count()
+
     %{
       part1: point_total,
-      part2: nil
+      part2: card_copy_total
     }
   end
 
